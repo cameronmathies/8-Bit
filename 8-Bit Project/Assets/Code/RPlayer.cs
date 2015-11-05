4b5161399b1691c380952a2f624d6344d8c379b2
cameronmathies <cmathies@IT005.mr.preble.com>
2015-11-05 13:28:15 -0500

    Player

---
+++ 8-Bit Project/Assets/Code/RPlayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RPlayer : MonoBehaviour {
+
+    public float moveSpeed;
+    public float speed = 10f;
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.LeftAlt))
+        {
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightAlt))
+        {
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+        }
+    }
+}
