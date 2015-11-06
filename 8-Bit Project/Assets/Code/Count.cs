6b5a2d87a9df4b39e13242044d54d6637c468ed8
cameronmathies <cmathies@IT005.mr.preble.com>
2015-11-06 13:18:42 -0500

    Timmer reset

---
+++ 8-Bit Project/Assets/Code/Count.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Count : MonoBehaviour {
+
+    float timeRemaining = 500 .0f; // Start time
+
+    void Update()
+    {
+        timeRemaining -= Time.deltaTime; //lowering the time
+    }
+
+    void OnGUI()
+    {
+        if (timeRemaining > 0)
+        {
+            GUI.Label(new Rect(100,100, 200, 100), // Ware to show text
+                         "Time Remaining : " + timeRemaining);
+        }
+        else
+        {
+            GUI.Label(new Rect(100, 100, 200, 100), "Time's Up"); //end
+            Application.LoadLevel("Main Menu");
+        }
+    }
+}
