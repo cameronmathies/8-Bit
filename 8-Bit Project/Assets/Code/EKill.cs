cd15ed0186786594e03ad849fdcf8a3ad0ee9f9a
cameronmathies <cmathies@IT005.mr.preble.com>
2015-11-13 12:09:45 -0500

    Goomba

---
+++ 8-Bit Project/Assets/Code/EKill.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EKill : MonoBehaviour {
+    public GameObject Goomba;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
